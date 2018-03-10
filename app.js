f6240169b5baf79939f15461a48ad5e19f8511a4
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-10 13:30:56 -0800

    Require blogRoutes in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose      = require('mongoose');
 const express       = require('express');
 const app           = express();
 const path          = require('path');
+const blogRoutes    = require('./routes/blog');
 
 // Connect to MongoDB
 const database = require('./config/database');
@@ -29,68 +30,9 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/post/add', (req, res) => {
+// Routes
+app.use('/blog', blogRoutes);
 
-    var BlogPost = require('./models/blogPostModel');
-
-    // Check if user is signed in
-
-    // Check if all required fields are filled
-
-    // Create new blogpost
-    var blog = new BlogPost({
-        title: 'Title',
-        body: 'Body',
-        author: 'Author',
-        likes: 0
-    });
-
-    // Save blog post
-    blog.save((err, blog) => {
-
-        if (err) {
-            res.status(500).send('Internal server error');
-        } else {
-            res.json({
-                status: 'success',
-                blog
-            });
-
-            // Render new pug template?
-        }
-    });
-});
-
-app.post('/edit/:id/:title/:body/:author', (req, res) => {
-
-    var BlogPost = require('./models/blogPostModel');
-    const {  id, title, body, author } = req.params;    
-
-    // Check if user is logged in
-
-    // Check if all required fields are filled
-
-    // Find blog post by id
-    BlogPost.findOne({ _id: id }, (err, blogPost) => {
-        if (err) {
-            res.status(500).send('Internal server error');
-        } else {
-            blogPost.title = title;
-            blogPost.body = body;
-
-            blogPost.save((err, updatedBlogPost) => {
-                if (err) {
-                    res.status(500).send('Internal server error');
-                } else {
-                    res.json(updatedBlogPost);
-                }
-            });
-        }
-    });
-
-    // Edit contents of blog
-
-});
 
 const port = 3000;
 app.listen(port, () => { console.log(`Now listening to Port ${port}`) });
