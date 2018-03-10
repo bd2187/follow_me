2c15c4661a2331874818c522dc91f373aaf7e2ec
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-10 14:55:02 -0800

    Save user to mongo db in /register post request

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    
+const User = require('../models/userModel');
+
+router.post(
+    '/register/:firstName/:lastName/:username/:password/:email/:confirmPassword',
+    (req, res) => {
+    const { firstName, lastName, username, password, email, confirmPassword } = req.params;
+    const user = new User({
+        firstName,
+        lastName,
+        username,
+        password,
+        email,
+        confirmPassword
+    });
+
+    user.save((err, user) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal server error.');
+        } else { 
+            res.json({
+                status: 'success',
+                user
+            });
+        }
+    });
 });
 
 router.post('/login/:id', (req, res) => {
