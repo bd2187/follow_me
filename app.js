2d257a065859207eb48d26e386a087c0fbf6f918
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-10 12:58:23 -0800

    Implement logic for querying blog by id and updating blog post

--- app.js
+++ app.js
@@ -64,12 +64,29 @@ app.post('/post/add', (req, res) => {
 app.post('/edit/:id/:title/:body/:author', (req, res) => {
 
     var BlogPost = require('./models/blogPostModel');
+    const {  id, title, body, author } = req.params;    
 
     // Check if user is logged in
 
     // Check if all required fields are filled
 
     // Find blog post by id
+    BlogPost.findOne({ _id: id }, (err, blogPost) => {
+        if (err) {
+            res.status(500).send('Internal server error');
+        } else {
+            blogPost.title = title;
+            blogPost.body = body;
+
+            blogPost.save((err, updatedBlogPost) => {
+                if (err) {
+                    res.status(500).send('Internal server error');
+                } else {
+                    res.json(updatedBlogPost);
+                }
+            });
+        }
+    });
 
     // Edit contents of blog
 
