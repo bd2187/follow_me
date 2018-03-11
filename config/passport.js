4e08f468619ceabda12524b557c2ac633c326b6f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-11 16:40:54 -0700

    Replace hash with user.password

--- config/passport.js
+++ config/passport.js
@@ -4,30 +4,41 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/userModel');
 
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-        User.findOne({username}, (err, user) => {
-            if (err) { return done(err); }
+module.exports = (passport) => {    
+    passport.use(new LocalStrategy(
+        (username, password, done) => {
+            User.findOne({username}, (err, user) => {
+                                
+                if (err) { return done(err); }
+    
+                if (!user) {
+                    console.log('Incorrect username');
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+    
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (!res) {
+                        console.log('incorrect password');
+                        return done(null, false, { message: 'Incorrect password.' });
+                    } else {
+                        return done(null, user);
+                    }
 
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-
-            bcrypt.compare(password, hash, (err, res) => {
-                return (password !== user.password) ? done(null, false, { message: 'Incorrect password.' }) : done(null, user);
+                });
             });
-        });
-
-        passport.serializeUser((user, done) => {
-            done(null, user.id);
-        });
-
-        passport.deserializeUser((id, done) => {
-            User.findById(id, (err, user) => {
-              done(err, user);
+    
+            passport.serializeUser((user, done) => {
+                done(null, user.id);
             });
-        });
-    }
-));
+    
+            passport.deserializeUser((id, done) => {
+                User.findById(id, (err, user) => {
+                  done(err, user);
+                });
+            });
+        }
+    ));
+};
 
-module.exports = passport;
+
+
