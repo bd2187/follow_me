9181c71108bce95952da81c848f8b3ce4ef29bdd
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-03-16 18:23:41 -0700

    Initial commit. Create User object and add event listeners to edit blog button

---
+++ public/javascript/main.js
@@ -0,0 +1,36 @@
+const User = {
+    init: function(USER = {}) {
+        
+        this.id = USER._id,
+        this.username = USER.username;        
+
+        const editBlogLinks = [...document.getElementsByClassName('edit-blog')];
+        editBlogLinks.forEach((el) => { this.editBlog(el) });
+    },
+
+    editBlog: function(el) {
+        el.addEventListener('click', function(e) {            
+            const editBlogModal = document.getElementById('editBlogModal');
+            const editBlogTitle = document.getElementsByClassName('edit-blog-title')[0];
+            const editBlogBody  = document.getElementsByClassName('edit-blog-body')[0];
+            
+            const blogFooter    = this.parentElement.parentElement.parentElement.parentElement;
+            const blogID        = blogFooter.getElementsByClassName('blog-id')[0].value;
+            const blogTitle     = blogFooter.getElementsByClassName('blog-title')[0].value;
+            const blogBody      = blogFooter.getElementsByClassName('blog-body')[0].value;
+
+            editBlogModal.addEventListener('click', function(e) {
+                if (e.target.id === 'editBlogModal') {
+                    this.style.display = 'none';
+                }                
+            });
+
+            editBlogModal.style.display = 'block';
+
+            // Populate modal
+            editBlogTitle.value = blogTitle;
+            editBlogBody.value = blogBody;
+
+        });
+    }
+}
